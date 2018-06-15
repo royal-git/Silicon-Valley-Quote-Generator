468fdedf74b013119d1e5ec6ba9358d29386a615
Dr Royal Thomas <royal.thomas@ucdconnect.ie>
2018-06-15 19:05:51 +0100

    Initial 'hello world' Setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# silicon_valley_quote_gen
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+main(List<String> args) {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        floatingActionButton: new FloatingActionButton(
+          child: new Icon(Icons.refresh),
+        ),
+        appBar: new AppBar(
+          title: new Text('Silicon Valley Quote Generator'),
+        ),
+        body: new Center(
+          child: new ListTile(
+            title: new Text("Hello"),
+            subtitle: new Text("World"),
+          ),
+        ),
+      ),
+    );
+  }
+}
